64964562c11a0691b24e8a5490711d3f7b414aab
conao3 <conao3@gmail.com>
2020-04-07 02:08:47 +0900

    remove advice function for imenu-list-update

--- leaf-tree.el
+++ leaf-tree.el
@@ -46,8 +46,7 @@
 ;;; Advice
 
 (defvar leaf-tree-advice-alist
-  '((imenu-list-collect-entries . leaf-tree--advice-imenu-list-collect-entries)
-    (imenu-list-update . leaf-tree--advice-imenu-list-update))
+  '((imenu-list-collect-entries . leaf-tree--advice-imenu-list-collect-entries))
   "Alist for leaf-tree advice.
 See `leaf-tree--setup' and `leaf-tree--teardown'.")
 
@@ -60,42 +59,6 @@ See `imenu-list-collect-entries'."
     (imenu-list-rescan-imenu)
     (setq imenu-list--imenu-entries imenu--index-alist)
     (setq imenu-list--displayed-buffer (current-buffer))))
-
-(defun leaf-tree--advice-imenu-list-update (fn &rest args)
-  "Around advice for FN with ARGS.
-This code based `imenu-list' (2019/03/15 hash:4600873)
-See `imenu-list-update'."
-  (if (not leaf-tree-mode)
-      (apply fn args)
-    (seq-let (raise-imenu-errors force-update) args
-      (catch 'index-failure
-        (let ((old-entries imenu-list--imenu-entries)
-              (location (point-marker)))
-          ;; don't update if `point' didn't move - fixes issue #11
-          (unless (and (null force-update)
-                       imenu-list--last-location
-                       (marker-buffer imenu-list--last-location)
-                       (= location imenu-list--last-location))
-            (setq imenu-list--last-location location)
-            (if raise-imenu-errors
-                (imenu-list-collect-entries)
-              (condition-case err
-                  (imenu-list-collect-entries)
-                (error
-                 (message "imenu-list: couldn't create index because of error: %S" err)
-                 (throw 'index-failure err))))
-            (when (or force-update
-                      ;; check if Ilist buffer is alive, in case it was killed
-                      ;; since last update
-                      (null (get-buffer imenu-list-buffer-name))
-                      (not (equal old-entries imenu-list--imenu-entries)))
-              (with-current-buffer (imenu-list-get-buffer-create)
-                (imenu-list-insert-entries)))
-            (imenu-list--show-current-entry)
-            (when imenu-list-auto-resize
-              (imenu-list-resize-window))
-            (run-hooks 'imenu-list-update-hook)
-            nil))))))
 
 
 ;;; Main
